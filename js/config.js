c2f259adce10bfed86bcff6fe2f35d0a2323ea3b
rinkuvantage <rinku.vantage@gmail.com>
2016-10-25 13:00:01 +0530

    updates

--- js/config.js
+++ js/config.js
@@ -15,81 +15,13 @@ function gup(sParam) {
     }
 }
 
-var user_id = gup('uid');
+var user_id = 0;
+if (localStorage.getItem('user_id') !== null) {
+	user_id = localStorage.getItem('user_id');
+}
 
-
-function checkloggedin(user_id)
-{
+function accountLogout(user_ids){
 	
-	var url = siteurl+'/api/register.php';
-	
-	jQuery.ajax({  
-	 type: 'POST',  
-	 url: url, 	
-	 dataType: 'json',  
-	 data: {account_type:'checkuserexist',uid:uid},  
-	 crossDomain: true,  
-	 beforeSend: function() {
-							
-	 },		
-	 complete: function() {
-		jQuery('body .bodyoverlay').remove();
-		jQuery('body .preloader').remove();					
-	 },
-	 success: function(res) {  
-	   if(res['success'][0]['message']=='Invalid userid')
-	   {
-		//window.location='index.html';   
-	   }
-		return false;
-	 },  
-	 error: function(response, d, a){
-		jQuery('footer .source').append('<span class="msgnotification">Server error</span>');
-		setTimeout(function(){
-			jQuery('footer .source span.msgnotification').remove();
-		},700);
-	 } 
-   });
-		
-}
-
-
-document.addEventListener("deviceready", init, false);
-function init() {
-	localStorage.setItem('device_id', device.uuid);	
-	setTimeout(function(){
-	document.querySelector("#applogout").addEventListener("touchend", applogout, false);
-						},1000);	
-}
-function applogout(){
-	TwitterConnect.logout(
-	  function() {
-		alert('Successful logout!');
-	  },
-	  function() {
-		alert('Error logging out');
-	  }
-	);
-	CordovaFacebook.logout({
-	   onSuccess: function() {
-		  alert("The user is now logged out");
-	   }
-	});
-}
-
-function getcountrycode(){
-	var requestUrl = "http://ip-api.com/json";
-	jQuery.ajax({
-	  url: requestUrl,
-	  type: 'GET',
-	  success: function(json)
-	  {
-		  var countrycode = json.countryCode;
-		  localStorage.setItem('countrycode',countrycode);
-		
-	  }
-	});
-}
-if(localStorage.getItem('countrycode')==null){
-	getcountrycode();
+	localStorage.removeItem('user_id');
+	location.reload();
 }
